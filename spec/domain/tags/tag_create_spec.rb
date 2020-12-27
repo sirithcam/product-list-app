a2bb1dfe7b092a09a8439414dc6e0f2d2127a859
Mateusz Czuchryta <mateusz.czuchryta@itmagination.com>
2020-12-27 12:16:42 +0100

    add domain class tests for tags

---
+++ spec/domain/tags/tag_create_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe Tags::Create do
+  let(:title)   { generate(:title) }
+  let(:product) { create(:product) }
+
+  describe '.run method' do
+    context 'valid attributes' do
+      it 'returns created tag' do
+        tag = Tags::Create.run(title)
+        expect(tag).to eq Tag.find_by_title(title)
+      end 
+    end 
+
+    context 'invalid attributes' do
+      it 'returns new not saved tag' do
+        tag = Tags::Create.run(' ')
+        expect(tag.id).to be_nil
+      end 
+    end 
+  end 
+end
